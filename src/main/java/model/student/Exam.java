57d0e3cf874f27c91ceac94abfd7a7200e6205f7
Catherine <sunny4evil@gmail.com>
2022-03-29 21:42:25 +0300

    fix(model): add constructors and getters to enums

--- src/main/java/model/student/Exam.java
+++ src/main/java/model/student/Exam.java
@@ -1,10 +1,21 @@
 package main.java.model.student;
 
 public enum Exam {
-    NO,
-    EGE_B,
-    EGE_P,
-    OGE,
-    VPR,
-    SESSION
+    NO(""),
+    EGE_B("EGE Base level"),
+    EGE_P("EGE Profile level"),
+    OGE("OGE"),
+    VPR("VPR"),
+    SESSION("Session exams"),
+    ENTRANCE("Entrance exams");
+
+    private String examName;
+
+    Exam(String examName) {
+        this.examName = examName;
+    }
+
+    public String getExamName() {
+        return examName;
+    }
 }
--- src/main/java/model/student/Zone.java
+++ src/main/java/model/student/Zone.java
@@ -1,12 +1,28 @@
 package main.java.model.student;
 
 public enum Zone {
-    MOSCOW,
-    MOSCOW_m1,
-    MOSCOW_m2,
-    MOSCOW_p1,
-    MOSCOW_p2,
-    MOSCOW_p3,
-    MOSCOW_p4,
-    MOSCOW_p5
+    MOSCOW(0, ""),
+    MOSCOW_m1(-1, "-1"),
+    MOSCOW_m2(-2,"-2"),
+    MOSCOW_p1(1, "+1"),
+    MOSCOW_p2(2, "+2"),
+    MOSCOW_p3(3, "+3"),
+    MOSCOW_p4(4, "+4"),
+    MOSCOW_p5(5, "+5");
+
+    private Integer zoneInt;
+    private String zoneStr;
+
+    Zone(Integer zoneInt, String zoneStr) {
+        this.zoneInt = zoneInt;
+        this.zoneStr = zoneStr;
+    }
+
+    public Integer getZoneInt() {
+        return zoneInt;
+    }
+
+    public String getZoneStr() {
+        return zoneStr;
+    }
 }
